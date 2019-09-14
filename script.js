443a432ac823a7d814f3432bac1a581fe3baa024
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-09-14 20:03:11 -0300

    using a Object Json for best read the code and make a system of modal

--- script.js
+++ script.js
@@ -6,14 +6,64 @@ class Modal{
     }
 
     aparecerModal(){
-        return this.page.style.display = 'Block';
+        return this.page.style.display = 'block';
+    }
+
+    desparecerModal(){
+        return this.page.style.display = 'none';
     }
 }
 
-const btnModal = window.document.querySelector('.btn-modal');
-const modal = window.document.querySelector('.modal');
+const components = {
+    btnModal: window.document.querySelector('.btn-modal'),
+    btnModal2: window.document.querySelector('.btn-modal2'),
+    btnModal3: window.document.querySelector('.btn-modal3'),
+    modal: window.document.querySelector('.modal'),
+    btnExitModal: window.document.querySelector('.exitModal'),
+    img01: window.document.querySelector('#img01'),
+    img02: window.document.querySelector('#img02'),
+    img03: window.document.querySelector('#img03'),
+    img04: window.document.querySelector('#img04'),
+    img05: window.document.querySelector('#img05'),
+    img06: window.document.querySelector('#img06'),
+    img07: window.document.querySelector('#img07'),
+    img08: window.document.querySelector('#img08'),
+    img09: window.document.querySelector('#img09'),
+};
 
-btnModal.addEventListener('click', () =>{
-    let modalObj = new Modal(modal);
+components.btnModal.addEventListener('click', () =>{
+    let modalObj = new Modal(components.modal);
     modalObj.aparecerModal();
+    
+    let antiscroll = window.document.querySelector('html, body');
+    antiscroll.style.overflow = 'hidden';
+    antiscroll.style.height = '100%';
 });
+
+components.btnModal2.addEventListener('click', () =>{
+    let modalObj = new Modal(components.modal);
+    modalObj.aparecerModal();
+    
+    let antiscroll = window.document.querySelector('html, body');
+    antiscroll.style.overflow = 'hidden';
+    antiscroll.style.height = '100%';
+});
+
+components.btnModal3.addEventListener('click', ()=>{
+    let modalObj = new Modal(components.modal);
+    modalObj.aparecerModal();
+    
+    let antiscroll = window.document.querySelector('html, body');
+    antiscroll.style.overflow = 'hidden';
+    antiscroll.style.height = '100%';
+});
+
+components.btnExitModal.addEventListener('click', ()=>{
+    let modalObj = new Modal(components.modal);
+    modalObj.desparecerModal();
+    let antiscroll = window.document.querySelector('html, body');
+    antiscroll.style.overflow = 'auto';
+    antiscroll.style.height = 'auto';
+});
+
+
