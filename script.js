68b6acc95541cdc581e5f17e741072c0eefa71c7
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-09-13 23:41:54 -0300

    creating a script for modal

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+class Modal{
+    constructor(_page){
+        this.page = _page;
+    }
+
+    aparecerModal(){
+        return this.page.style.display = 'Block';
+    }
+}
+
+const btnModal = window.document.querySelector('.btn-modal');
+const modal = window.document.querySelector('.modal');
+
+btnModal.addEventListener('click', () =>{
+    let modalObj = new Modal(modal);
+    modalObj.aparecerModal();
+});
